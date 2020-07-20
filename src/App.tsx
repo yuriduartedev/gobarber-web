b86d8ca49e55f15e12ceee94388161d2bf6740ac
Yuri Duarte <yuriduartedev@gmail.com>
2020-07-20 10:19:37 -0300

    Configuração de estilos globais do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello App</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello App</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
